d29bdd6531c1cbac2395ba83a9a462d2aa047f8f
lzyprime <2383518170@qq.com>
2018-10-05 22:23:17 +0800

    django blogtest learn

---
+++ blogtest/blog/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Artice
+
+admin.site.register(Artice)
---
+++ blogtest/blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+from .models import Artice
+
+
+def index(request):
+    articles = Artice.objects.all()
+    return render(request, 'blog/index.html', {'articles': articles})
+
+def article_page(request, article_id):
+    article = Artice.objects.get(pk = article_id)
+    return render(request, 'blog/articlePage.html', {'article' : article})
